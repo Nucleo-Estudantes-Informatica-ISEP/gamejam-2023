a9275c0ab47b9c0080aaf25918290da907a93e3f
Eduardoooxd <ecouto93@gmail.com>
2023-05-13 12:05:48 +0100

    chore: Adding configuration to deploy on netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify/functions';
 
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel()
+  output: 'server',
+  adapter: netlify()
 });
