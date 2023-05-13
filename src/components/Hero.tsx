4b40d2185462b0ea575aea1d86fc8f9e08dc5ef8
André Barros <1211299@isep.ipp.pt>
2023-05-13 20:10:33 +0100

    chore: remvoved background from counter component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,4 @@
-import { motion, useScroll, useTransform } from 'framer-motion';
+import { filterProps, motion, useScroll, useTransform } from 'framer-motion';
 import HeroParticles from './HeroParticles';
 
 const Hero: React.FC = () => {
