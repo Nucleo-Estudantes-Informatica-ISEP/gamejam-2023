fb9bf3e2839fc7d8c9c67d9bd6f40c6dc4f87055
Tomás Lopes <1211289@isep.ipp.pt>
2023-05-18 19:02:37 +0100

    fix: fixed font size on mobile

--- src/components/AwardsContent.tsx
+++ src/components/AwardsContent.tsx
@@ -5,21 +5,21 @@ const AwardsContent: React.FC = () => {
         <h5 className="text-6xl my-2 text-center text-yellow-400">1º Lugar</h5>
         <div className="flex flex-col justify-center items-center relative hover:scale-110 hover:drop-shadow-gold duration-150 ease-in-out">
           <img className="max-h-96  w-full" src="/prize-base.png" alt="Prémio de primeiro lugar" />
-          <p className="text-center text-5xl lg:text-6xl absolute bottom-1/4">500€</p>
+          <p className="text-center text-4xl lg:text-6xl absolute bottom-1/4">500€</p>
         </div>
       </div>
       <div className="flex flex-col justify-center my-4 mx-10">
         <h5 className="text-6xl my-2 text-center text-slate-500">2º Lugar</h5>
         <div className="flex flex-col justify-center items-center relative hover:scale-110 hover:drop-shadow-silver duration-150 ease-in-out">
           <img className="max-h-96  w-full" src="/prize-base.png" alt="Prémio de segundo lugar" />
-          <p className="text-center text-5xl lg:text-6xl absolute bottom-1/4">350€</p>
+          <p className="text-center text-4xl lg:text-6xl absolute bottom-1/4">350€</p>
         </div>
       </div>
       <div className="flex flex-col justify-center my-4 mx-10">
         <h5 className="text-6xl my-2 text-center mb-2 text-amber-600">3º Lugar</h5>
         <div className="flex flex-col justify-center items-center relative hover:scale-110 hover:drop-shadow-bronze duration-150 ease-in-out">
           <img className="max-h-96 w-full" src="/prize-base.png" alt="Prémio de terceiro lugar" />
-          <p className="text-center text-5xl lg:text-6xl absolute bottom-1/4">150€</p>
+          <p className="text-center text-4xl lg:text-6xl absolute bottom-1/4">150€</p>
         </div>
       </div>
     </div>
