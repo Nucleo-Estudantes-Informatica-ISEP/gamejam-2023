ecd5816340b758d0a5cef68563d1b8df8a73251f
Tomás Lopes <1211289@isep.ipp.pt>
2023-05-18 18:52:16 +0100

    fix: fixed awards

--- src/components/AwardsContent.tsx
+++ src/components/AwardsContent.tsx
@@ -3,36 +3,24 @@ const AwardsContent: React.FC = () => {
     <div className="flex flex-col md:flex-row w-5/6 justify-center md:justify-around items-center mt-5">
       <div className="flex flex-col justify-center my-4 mx-10">
         <h5 className="text-6xl my-2 text-center text-yellow-400">1º Lugar</h5>
-        <div className="flex flex-col justify-center items-center">
-          <img
-            className="max-h-72 max-w-full h-auto w-full"
-            src="/prize-base.png"
-            alt="Premio de primeiro lugar"
-          />
-          <p className="text-center text-2xl md:text-6xl">500€</p>
+        <div className="flex flex-col justify-center items-center relative">
+          <img className="max-h-96  w-full" src="/prize-base.png" alt="Premio de primeiro lugar" />
+          <p className="text-center text-5xl lg:text-6xl absolute bottom-1/4">500€</p>
         </div>
       </div>
-      <div className="flex flex-col justify-start my-4 mx-10">
+      <div className="flex flex-col justify-center my-4 mx-10">
         <h5 className="text-6xl my-2 text-center text-slate-500">2º Lugar</h5>
-        <div className="flex flex-col justify-center items-center">
-          <img
-            className="max-h-72 max-w-full h-auto w-full"
-            src="/prize-base.png"
-            alt="Premio de segundo lugar"
-          />
-          <p className="text-center text-2xl md:text-6xl">350€</p>
+        <div className="flex flex-col justify-center items-center relative">
+          <img className="max-h-96  w-full" src="/prize-base.png" alt="Premio de segundo lugar" />
+          <p className="text-center text-5xl lg:text-6xl absolute bottom-1/4">350€</p>
         </div>
       </div>
-      <div className="flex flex-col justify-start my-4 mx-10">
+      <div className="flex flex-col justify-center my-4 mx-10">
         <h5 className="text-6xl my-2 text-center mb-2 text-amber-600">3º Lugar</h5>
-        <div className="flex flex-col justify-center items-center">
-          <img
-            className="max-h-72 max-w-full h-auto w-full"
-            src="/prize-base.png"
-            alt="Premio de terceiro lugar"
-          />
+        <div className="flex flex-col justify-center items-center relative">
+          <img className="max-h-96 w-full" src="/prize-base.png" alt="Premio de terceiro lugar" />
+          <p className="text-center text-5xl lg:text-6xl absolute bottom-1/4">150€</p>
         </div>
-        <p className="text-center text-2xl md:text-6xl">150€</p>
       </div>
     </div>
   );
