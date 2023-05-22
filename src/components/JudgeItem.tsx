10547e9bef71e95123188ae1f85799dd1ded03c9
Tomás Lopes <1211289@isep.ipp.pt>
2023-05-22 22:04:09 +0100

    feat: added judge section

--- src/components/JudgeItem.tsx
+++ src/components/JudgeItem.tsx
@@ -2,12 +2,18 @@ interface JudgeItemProps {
   name: string;
   image: string;
   description: string;
+  sectionHovered: boolean;
 }
 
-const JudgeItem: React.FC<JudgeItemProps> = ({ name, image, description }) => {
+const JudgeItem: React.FC<JudgeItemProps> = ({ name, image, description, sectionHovered }) => {
   return (
-    <article className="bg-gray-600 h-full max-h-40 w-full">
-      <h3>Judge name</h3>
+    <article
+      className={`bg-gray-600 w-full rounded-lg px-4 py-6 cursor-pointer hover:scale-105 hover:brightness-100 duration-100 ease-in-out ${
+        sectionHovered && 'brightness-50'
+      }`}>
+      <img src={image} alt={name} />
+      <h3 className="text-4xl">{name}</h3>
+      <p>{description}</p>
     </article>
   );
 };
--- src/components/Judges.tsx
+++ src/components/Judges.tsx
@@ -1,9 +1,41 @@
+import { useState } from 'react';
 import JudgeItem from './JudgeItem';
 
 const Judges: React.FC = () => {
+  const [sectionHovered, setSectionHovered] = useState(false);
+
   return (
-    <section className="w-full gap-8 items-center justify-around flex flex-row">
-      <JudgeItem name="name" description="description" image="#" />
+    <section
+      onMouseOver={() => {
+        console.log('hover');
+        setSectionHovered(true);
+      }}
+      onMouseOut={() => setSectionHovered(false)}
+      className="w-full gap-8 items-center justify-around flex flex-row py-4">
+      <JudgeItem
+        name="name"
+        description="description"
+        image="http://github.com/tomasflopes.png"
+        sectionHovered={sectionHovered}
+      />
+      <JudgeItem
+        name="name"
+        description="description"
+        image="http://github.com/tomasflopes.png"
+        sectionHovered={sectionHovered}
+      />
+      <JudgeItem
+        name="name"
+        description="description"
+        image="http://github.com/tomasflopes.png"
+        sectionHovered={sectionHovered}
+      />
+      <JudgeItem
+        name="name"
+        description="description"
+        image="http://github.com/tomasflopes.png"
+        sectionHovered={sectionHovered}
+      />
     </section>
   );
 };
