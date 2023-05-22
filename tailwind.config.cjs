4b9359267069722550328770362bbb9f14b6342f
Tomás Lopes <1211289@isep.ipp.pt>
2023-05-22 17:14:19 +0100

    faet: added registration section

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,6 +13,7 @@ module.exports = {
       colors: {
         'background-light': '#132C36',
         background: '#0d1e25',
+        contrast: '#21EFC2',
         primary: '#B567DC',
         secondary: '#3b82f6',
         bronze: '#cd7f32',
@@ -33,7 +34,9 @@ module.exports = {
         silver: '1px 1px 4px #c0c0c0',
         bronze: '1px 1px 4px #cd7f32',
         whiteStrokeShadow: '2px 2px 0 #fff',
-        primaryStrokeShadow: '3px 3px 0 #B567DC'
+        primaryStrokeShadow: '3px 3px 0 #B567DC',
+        buttonShadow: '2px 2px 10px #B567DC',
+        buttonShadowHover: '3px 3px 15px #B567DC'
       }
     }
   },
