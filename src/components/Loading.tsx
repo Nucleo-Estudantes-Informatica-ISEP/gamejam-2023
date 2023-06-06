8bd1cc4dd299ddb10e7ac6d2007505129aa4ce05
Tomás Lopes <1211289@isep.ipp.pt>
2023-06-06 17:38:13 +0100

    fix: fixed loading animationgp

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion';
 
 const Loading: React.FC = () => {
   return (
-    <section className="w-full flex flex-col items-center justify-center mb-24">
+    <section className="w-full flex flex-col items-center justify-center mb-32 relative">
       <h1 className="font-black uppercase text-5xl md:text-6xl lg:text-[9rem] text-primary font-misterPixel drop-shadow-whiteStrokeShadow">
         Loading
       </h1>
@@ -28,15 +28,29 @@ const Loading: React.FC = () => {
           className="w-full h-full bg-gradient-to-r from-secondary to-primary rounded-full"></motion.div>
       </motion.div>
       <motion.h1
-        className="-top-24 text-white font-black text-5xl md:text-6xl lg:text-[9rem] text-center font-misterPixel uppercase drop-shadow-primaryStrokeShadow"
+        className="absolute bottom-0 translate-y-full text-white font-black text-5xl md:text-6xl lg:text-[9rem] text-center font-misterPixel uppercase drop-shadow-primaryStrokeShadow"
+        initial={{
+          scale: 0,
+          opacity: 0
+        }}
         whileInView={{
           scale: [0, 1],
           opacity: [0, 1]
         }}
+        animate={{
+          translateY: ['0', '10px', '0']
+        }}
         viewport={{ once: true }}
         transition={{
           delay: 5,
-          duration: 0.5
+          duration: 0.5,
+          ease: 'easeInOut',
+          translateY: {
+            duration: 1,
+            repeat: Infinity,
+            repeatType: 'reverse',
+            ease: 'easeInOut'
+          }
         }}>
         Até para o ano!
       </motion.h1>
